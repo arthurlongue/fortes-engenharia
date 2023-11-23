bf33cb21364f4a34214af22438e817b15d3ff6eb
Arthur Longue <arthurlongue@hotmail.com>
2023-11-23 12:06:47 -0300

    hero changes and img replacemente

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,29 +1,28 @@
 import React from "react"
 
 function Hero() {
-  return (
-    <div className="hero min-h-screen bg-green-50 text-slate-700">
-      <div className="hero-content  min-w-full bg-white py-20 text-center sm:min-w-0 sm:rounded-3xl sm:p-20">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">
-            Olá Usuário,
-          </h1>
-          <h1 className="text-5xl font-bold">
-            bem-vindo ao Ponto Fortes.
-          </h1>
-          <p className="py-6">
-            Abaixo você encontra os projetos que a Fortes
-            Engenharia está desenvolvendo!
-          </p>
-          <a href="#cards-grid">
-            <button className={`btn-primary btn`}>
-              VER PROJETOS
-            </button>
-          </a>
-        </div>
-      </div>
+ return (
+  <>
+   <div
+    className="hero min-h-screen"
+    style={{ backgroundImage: "url(/wallpaper-engenharia.jpg)" }}>
+    <div className="hero-overlay bg-opacity-60"></div>
+    <div className="hero-content text-center ">
+     <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Olá Usuário,</h1>
+      <h1 className="text-5xl font-bold">bem-vindo ao Ponto Fortes.</h1>
+      <p className="mb-5">
+       Abaixo você encontra os projetos que a Fortes Engenharia está
+       desenvolvendo!
+      </p>
+      <a href="#cards-grid">
+       <button className={`btn-primary btn bg-sky-500`}>VER PROJETOS</button>
+      </a>
+     </div>
     </div>
-  )
+   </div>
+  </>
+ )
 }
 
 export default Hero
