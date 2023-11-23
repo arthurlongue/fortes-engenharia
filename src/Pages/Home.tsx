003e21463cd0c7e59ea1e96a4fb53dc8d482f45f
Arthur Longue <arthurlongue@hotmail.com>
2023-11-23 12:11:54 -0300

    modal background color changes

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,36 +1,36 @@
 import React from "react"
-import Header from "../components/Header/Header"
-import Footer from "../components/Footer/Footer"
-import Hero from "../components/Hero/Hero"
-import Card from "../components/Card/Card"
 import cardData from "../cardData"
+import Card from "../components/Card/Card"
+import Footer from "../components/Footer/Footer"
+import Header from "../components/Header/Header"
+import Hero from "../components/Hero/Hero"
 import ModalProject from "../components/Modal/ModalProject"
 
 function Home() {
-  return (
-    <div className="bg-slate-100 text-white">
-      <Header></Header>
-      <Hero></Hero>
+ return (
+  <div className="bg-slate-100 text-white">
+   <Header></Header>
+   <Hero></Hero>
 
-      <div
-        id="cards-grid"
-        className="flex flex-wrap place-content-center bg-sky-600">
-        <div className="my-8 flex min-h-max w-screen bg-sky-600">
-          <ModalProject></ModalProject>
-        </div>
-
-        {cardData.map((card) => (
-          <Card
-            key={card.id}
-            {...card}
-            button={["Equipe Fortes", "Projetista"]}
-            color={["", "bg-sky-600"]}
-          />
-        ))}
-      </div>
-      <Footer></Footer>
+   <div
+    id="cards-grid"
+    className="flex flex-wrap place-content-center bg-sky-400">
+    <div className="my-8 flex min-h-max w-screen">
+     <ModalProject></ModalProject>
     </div>
-  )
+
+    {cardData.map((card) => (
+     <Card
+      key={card.id}
+      {...card}
+      button={["Equipe Fortes", "Projetista"]}
+      color={["", "bg-sky-600"]}
+     />
+    ))}
+   </div>
+   <Footer></Footer>
+  </div>
+ )
 }
 
 export default Home
--- src/components/Modal/ModalProject.tsx
+++ src/components/Modal/ModalProject.tsx
@@ -1,56 +1,48 @@
 import React from "react"
 
 function ModalProject() {
-  return (
-    <>
-      <label
-        htmlFor="my-modal-8"
-        className="btn m-auto  bg-green-400 text-3xl text-white shadow-lg hover:scale-110  hover:bg-green-300">
-        Novo Projeto
+ return (
+  <>
+   <label
+    htmlFor="my-modal-8"
+    className="btn m-auto  bg-green-500 text-3xl text-white shadow-lg hover:scale-110  hover:bg-green-400">
+    Criar Projeto
+   </label>
+   <input type="checkbox" id="my-modal-8" className="modal-toggle" />
+   <div className="modal modal-bottom  sm:modal-middle">
+    <div className="modal-box bg-white text-slate-600">
+     <h3 className="mb-4 text-lg font-bold">
+      Insira os dados do novo projeto:
+     </h3>
+     <ModalInput text="Nome do Projeto:"></ModalInput>
+     <ModalInput text="Status:"></ModalInput>
+     <ModalInput text="Cliente:"></ModalInput>
+     <ModalInput text="Data:"></ModalInput>
+     <ModalInput text="TAGS:"></ModalInput>
+     <div className="modal-action mt-0">
+      <label htmlFor="my-modal-8" className="btn bg-green-400 text-white">
+       Voltar
       </label>
-      <input
-        type="checkbox"
-        id="my-modal-8"
-        className="modal-toggle"
-      />
-      <div className="modal modal-bottom  sm:modal-middle">
-        <div className="modal-box bg-white text-slate-600">
-          <h3 className="mb-4 text-lg font-bold">
-            Insira os dados do novo projeto:
-          </h3>
-          <ModalInput text="Nome do Projeto:"></ModalInput>
-          <ModalInput text="Status:"></ModalInput>
-          <ModalInput text="Cliente:"></ModalInput>
-          <ModalInput text="Data:"></ModalInput>
-          <ModalInput text="TAGS:"></ModalInput>
-          <div className="modal-action mt-0">
-            <label
-              htmlFor="my-modal-8"
-              className="btn bg-green-400 text-white">
-              Voltar
-            </label>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+     </div>
+    </div>
+   </div>
+  </>
+ )
 }
 
 function ModalInput({ text }) {
-  return (
-    <div className="form-control w-full max-w-xs">
-      <label className="label">
-        <span className="label-text text-slate-700">
-          {text}
-        </span>
-      </label>
-      <input
-        type="text"
-        placeholder="Digite aqui"
-        className="input-bordered input w-full max-w-xs bg-slate-50"
-      />
-    </div>
-  )
+ return (
+  <div className="form-control w-full max-w-xs">
+   <label className="label">
+    <span className="label-text text-slate-700">{text}</span>
+   </label>
+   <input
+    type="text"
+    placeholder="Digite aqui"
+    className="input-bordered input w-full max-w-xs bg-slate-50"
+   />
+  </div>
+ )
 }
 
 export default ModalProject
